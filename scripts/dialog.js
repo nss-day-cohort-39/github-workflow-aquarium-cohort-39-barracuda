5d1e09caa6dc07bfd9d0ef048a3936e4b5b7ebf3
Andrew Harper <druharper@gmail.com>
2020-02-24 11:50:56 -0600

    bad changes

--- scripts/dialog.js
+++ scripts/dialog.js
@@ -1,56 +1,74 @@
 const initializeDetailButtonEvents = () => {
-  // CAN'T TOUCH THIS - START
-  const allCloseButtons = document.querySelectorAll(".button--close")
+    // CAN'T TOUCH THIS - START
+    const allCloseButtons = document.querySelectorAll(".button--close")
 
-  for (const btn of allCloseButtons) {
-    btn.addEventListener("click", theEvent => {
-      const dialogElement = theEvent.target.parentNode
-      dialogElement.close()
-    })
-  }
-  // CAN'T TOUCH THIS - END
+    for (const btn of allCloseButtons) {
+        btn.addEventListener("click", theEvent => {
+                const dialogElement = theEvent.target.parentNode
+                dialogElement.close()
+            }
+        )
+    }
+    // CAN'T TOUCH THIS - END
 
-  // You will be writing code below this line
 
-  // Show Bart's details when the button is clicked
-  document
-    .querySelector("#button--bart")
-    .addEventListener("click", theClickEvent => {
-      const theDialog = document.querySelector("#details--bart")
-      theDialog.showModal()
-    })
+    // You will be writing code below this line
 
-  // Show Betty's details when the button is clicked
-  document
-    .querySelector("#button--betty")
-    .addEventListener("click", theClickEvent => {
-      const theDialog = document.querySelector("#details--betty")
-      theDialog.showModal()
-    })
+    // Show Bart's details when the button is clicked
+    document.querySelector("#button--bart").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--bart")
+            theDialog.showModal()
+        }
+    )
 
-  // Show Nemo's details when the button is clicked
-  document
-    .querySelector("#button--nemo")
-    .addEventListener("click", theClickEvent => {
-      const theDialog = document.querySelector("#details--nemo")
-      theDialog.showModal()
-    })
+    // Show Betty's details when the button is clicked
+    document.querySelector("#button--betty").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--betty")
+            theDialog.showModal()
+        }
+    )
 
-  // Show Dory's details when the button is clicked
-  document
-    .querySelector("#button--dory")
-    .addEventListener("click", theClickEvent => {
-      const theDialog = document.querySelector("#details--dory")
-      theDialog.showModal()
-    })
+    // Show Nemo's details when the button is clicked
+    document.querySelector("#button--nemo").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--nemo")
+            theDialog.showModal()
+        }
+    )
 
-  // Show barracuda's details when the button is clicked
-  document
-    .querySelector("#button--barracuda")
-    .addEventListener("click", theClickEvent => {
-      const theDialog = document.querySelector("#details--barracuda")
-      theDialog.showModal()
-    })
+    // Show Dory's details when the button is clicked
+    document.querySelector("#button--dory").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--dory")
+            theDialog.showModal()
+        }
+    )
+    // Shows Albert's details when the button is called
+    document.querySelector("#button--albert").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--albert")
+            theDialog.showModal()
+        }
+    )
+
+    // Show Trey's details when the button is clicked
+document.querySelector("#button--trey").addEventListener(
+    "click",
+    theClickEvent => {
+        const theDialog = document.querySelector("#details--trey")
+        theDialog.showModal()
+    }
+)
+    
 }
+
+
 
 export default initializeDetailButtonEvents
