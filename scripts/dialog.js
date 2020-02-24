aded7e58a93c66ec7518b0dbd0b6b11eb0411135
Andrew Harper <druharper@gmail.com>
2020-02-24 13:07:04 -0600

    Barry the Barracuda added

--- scripts/dialog.js
+++ scripts/dialog.js
@@ -52,13 +52,13 @@ const initializeDetailButtonEvents = () => {
 
 
 // Show Thanos's details when the button is clicked
-document.querySelector("#button--thanos").addEventListener(
-    "click",
-    theClickEvent => {
-        const theDialog = document.querySelector("#details--thanos")
-        theDialog.showModal()
-    }
-)
+    document.querySelector("#button--thanos").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--thanos")
+            theDialog.showModal()
+        }
+    )
     // Shows Albert's details when the button is called
     document.querySelector("#button--albert").addEventListener(
         "click",
@@ -69,13 +69,21 @@ document.querySelector("#button--thanos").addEventListener(
     )
 
     // Show Trey's details when the button is clicked
-document.querySelector("#button--trey").addEventListener(
-    "click",
-    theClickEvent => {
-        const theDialog = document.querySelector("#details--trey")
-        theDialog.showModal()
-    }
-)
+    document.querySelector("#button--trey").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--trey")
+            theDialog.showModal()
+        }
+    )
+
+    document.querySelector("#button--barracuda").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--barracuda")
+            theDialog.showModal()
+        }
+    )
     
 }
 
